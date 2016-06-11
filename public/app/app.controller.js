afc571c3f6271753d9f839cf1eb6c837d006625d
Forrest Collins <Forest.D.Collins@gmail.com>
2016-06-11 00:24:23 -0500

    Added an auth-check on the main-application controller and restricted some features

--- public/app/app.controller.js
+++ public/app/app.controller.js
@@ -5,12 +5,24 @@
 	.controller('AppController', AppController);
 
 	/* @ngInject */
-	function AppController ($state, $scope, $mdSidenav) {
+	function AppController ($state, $scope, $rootScope, $mdSidenav, appAuth) {
 		var vm = this;
 		vm.openUserMenu = openUserMenu;
+		vm.updateLoginStatus = updateLoginStatus;
 
 		function openUserMenu() {
 			$mdSidenav('left').toggle();
 		}
+
+		function updateLoginStatus() {
+			vm.isLoggedIn = appAuth.isLoggedIn();
+			vm.user = appAuth.getUser();
+		}
+
+		$rootScope.$on('loggedIn', function() {
+			updateLoginStatus();
+		});
+
+		updateLoginStatus();
 	}
 }());
