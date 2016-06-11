d97851360354c1a80282bb62ad2838340902e7ae
Forrest Collins <Forest.D.Collins@gmail.com>
2016-06-11 02:09:44 -0500

    Styling has been done to the signup and login pages

--- public/app/app.controller.js
+++ public/app/app.controller.js
@@ -5,10 +5,11 @@
 	.controller('AppController', AppController);
 
 	/* @ngInject */
-	function AppController ($state, $scope, $rootScope, $mdSidenav, appAuth) {
+	function AppController ($state, $scope, $rootScope, $mdSidenav, appAuth, appStorage) {
 		var vm = this;
 		vm.openUserMenu = openUserMenu;
 		vm.updateLoginStatus = updateLoginStatus;
+		vm.logout = logout;
 
 		function openUserMenu() {
 			$mdSidenav('left').toggle();
@@ -19,7 +20,17 @@
 			vm.user = appAuth.getUser();
 		}
 
+		function logout() {
+			appStorage.remove('user');
+			appStorage.remove('opinion-token');
+			$scope.$broadcast('loggedOut');
+		}
+
 		$rootScope.$on('loggedIn', function() {
+			updateLoginStatus();
+		});
+
+		$scope.$on('loggedOut', function() {
 			updateLoginStatus();
 		});
 
