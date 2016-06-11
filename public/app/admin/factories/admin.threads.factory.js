fe9f049111acd2e8e3ed66cb8b43ae42351e562d
Forrest Collins <Forest.D.Collins@gmail.com>
2016-06-11 04:44:25 -0500

    Client side thread resource added

---
+++ public/app/admin/factories/admin.threads.factory.js
@@ -0,0 +1,15 @@
+(function() {
+	'use strict';
+
+	angular.module('opinionated.admin')
+	.factory('appThreads', appThreads);
+
+	/* @ngInject */
+	function appThreads ($resource) {
+		return {
+			single: $resource('threads/:threadId/:action', {
+				threadId: '@_id'
+			})
+		};
+	}
+}());
