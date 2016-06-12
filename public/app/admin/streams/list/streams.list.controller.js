b2436e7a402d2aae0b03b8de915aea708374134c
Forrest Collins <Forest.D.Collins@gmail.com>
2016-06-11 22:21:26 -0500

    Quite a bit more styling done to the streams-list page, this is always the longest part

--- public/app/admin/streams/list/streams.list.controller.js
+++ public/app/admin/streams/list/streams.list.controller.js
@@ -5,7 +5,7 @@
 	.controller('StreamsListController', StreamsListController);
 
 	/* @ngInject */
-	function StreamsListController ($state, $scope, $location, appStreams) {
+	function StreamsListController ($state, $scope, $location, $timeout, appStreams) {
 		var vm = this;
 		vm.streams = [];
 		vm.getStreams = getStreams;
@@ -29,20 +29,20 @@
 		}
 
 		function subscribe (item) {
-			item.subscribed = true;
+			
 			var stream = appStreams.single.get({streamId: item._id}, function() {
 				stream.$subscribe({streamId: item._id}, function() {
 					angular.extend(item, stream.res.record);
-					
+					item.subscribed = true;
 				});
 			});
 		}
 
 		function unsubscribe (item) {
-			item.subscribed = false;
 			var stream = appStreams.single.get({streamId: item._id}, function() {
 				stream.$unsubscribe({streamId: item._id}, function() {
 					angular.extend(item, stream.res.record);
+					item.subscribed = false;
 					
 				});
 			});
