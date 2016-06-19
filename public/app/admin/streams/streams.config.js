cb45be744aaf25ba1cfe78e6a7c6ab7741c7f4a3
Forrest Collins <Forest.D.Collins@gmail.com>
2016-06-19 18:25:32 -0500

    Added a sass file for animations and created some mixins

--- public/app/admin/streams/streams.config.js
+++ public/app/admin/streams/streams.config.js
@@ -6,6 +6,11 @@
 
 	/* @ngInject */
 	function streamsConfig ($stateProvider, $urlRouterProvider) {
+		$stateProvider.state('streams', {
+			templateUrl: '/app/admin/streams/streams.tmpl.html',
+			abstract: true
+		});
+
 		$stateProvider.state('streamsCreate', {
 			url: '/create-stream',
 			templateUrl: '/app/admin/streams/create/streams.create.tmpl.html',
@@ -13,28 +18,28 @@
 			controllerAs: 'vm'
 		});
 
-		$stateProvider.state('streamsList', {
+		$stateProvider.state('streasm.streamsList', {
 			url: '/streams/list',
 			templateUrl: '/app/admin/streams/list/streams.list.tmpl.html',
 			controller: 'StreamsListController',
 			controllerAs: 'vm'
 		});
 
-		$stateProvider.state('streamsSingle', {
+		$stateProvider.state('streams.streamsSingle', {
 			url: '/streams/:streamId',
 			templateUrl: '/app/admin/streams/single/streams.single.tmpl.html',
 			controller: 'StreamsSingleController',
 			controllerAs: 'vm'
 		});
 
-		$stateProvider.state('streamsEdit', {
+		$stateProvider.state('streams.streamsEdit', {
 			url: '/streams/edit/:streamId',
 			templateUrl: '/app/admin/streams/edit/streams.edit.tmpl.html',
 			controller: 'StreamsEditController',
 			controllerAs: 'vm'
 		});
 		
-		$stateProvider.state('streamsThread', {
+		$stateProvider.state('streams.streamsThread', {
 			url: '/streams/:streamId/:threadId',
 			templateUrl: '/app/admin/threads/thread.tmpl.html',
 			controller: 'ThreadController',
