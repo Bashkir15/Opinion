718b5757359fb5415e0c86a54e67249e9f6b1717
Forrest Collins <Forest.D.Collins@gmail.com>
2016-06-19 19:00:44 -0500

    Added a password reset dialog to the login view

--- public/app/authentication/login/login.controller.js
+++ public/app/authentication/login/login.controller.js
@@ -5,11 +5,12 @@
 	.controller('LoginController', LoginController);
 
 	/* @ngInject */
-	function LoginController ($state, $scope, $rootScope, appStorage, appToast, appUsers) {
+	function LoginController ($state, $scope, $rootScope, $mdDialog, appStorage, appToast, appUsers) {
 		var vm = this;
 		vm.reset = reset;
 		vm.login = login;
 		vm.postLogin = postLogin;
+		vm.openResetPassword = openResetPassword;
 		vm.user = {
 			email: '',
 			password: ''
@@ -48,6 +49,30 @@
 			appStorage.set('opinion-token', token);
 			$rootScope.$broadcast('loggedIn');
 			$state.go('home');
+		}
+
+		function openResetPassword() {
+			$mdDialog.show({
+				controller: [
+					'$scope',
+					'$mdDialog',
+					function ($scope, $mdDialog) {
+
+						$scope.reset = function() {
+							passwordResetForm.$setPristine();
+							passwordResetForm.$setUntouched();
+							$scope.email = '';
+						};
+
+						$scope.close = function() {
+							$mdDialog.hide();
+						};
+					}
+				],
+				templateUrl: '/app/authentication/reset/password.reset.tmpl.html'
+			}).finally(function() {
+
+			});
 		}
 	}
 }());
