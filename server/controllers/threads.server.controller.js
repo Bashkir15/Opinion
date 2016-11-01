3a64ca33f718244562d9543fa9ae8c980e3a4571
Forrest Collins <Forest.d.collins@gmail.com>
2016-10-31 23:22:01 -0500

    The server code for liking a thread is done

--- server/controllers/threads.server.controller.js
+++ server/controllers/threads.server.controller.js
@@ -11,6 +11,7 @@ module.exports = () => {
 		var thread = new Thread(req.body);
 		thread.stream = req.body.stream;
 		thread.creator = req.user._id;
+		thread.likes.push(req.user._id);
 		thread.save((err) => {
 
 			Stream.findOne({_id: req.body.stream}, (err, stream) => {
@@ -44,17 +45,34 @@ module.exports = () => {
 		var getPosts = () => {
 			var criteria = {stream: streamId};
 
+			if (req.query && req.query.timestamp) {
+				criteria.created = {
+					$gte: req.query.timestamp
+				};
+			}
+
+			if (req.query && req.query.filter) {
+				delete critera.created;
+				criteria.title = new RegExp(req.query.filter, 'i');
+			}
+
 			Thread.find(criteria)
 			.populate('creator')
 			.populate('stream')
 			.exec((err, threads) => {
 				if (err) {
 					return json.bad(err, res);
-				}
+				} else {
+					if (req.user) {
+						threads.map((e) => {
+							e = e.afterSave(req.user);
+						});
+					}
 
-				return json.good({
-					records: threads
-				}, res);
+					return json.good({
+						records: threads
+					}, res);
+				}
 			});
 		};
 
@@ -71,9 +89,53 @@ module.exports = () => {
 				return json.bad(err, res);
 			}
 
+			if (req.user) {
+				thread = thread.afterSave(req.user);
+			}
+
 			json.good({
 				record: thread
 			}, res);
+		});
+	};
+
+	obj.like = (req, res) => {
+		Thread.findOne({_id: req.params.threadId}, (err, thread) => {
+			if (err) {
+				return json.bad(err, res);
+			} else {
+				if (thread.likes.indexOf(req.user._id) !== -1) {
+					return json.bad({message: 'Sorry, you have already liked that thread'}, res);
+				} else if (thread.dislikes.indexOf(req.user._id) !== -1) {
+					thread.dislikes.splice(thread.dislikes.indexOf(req.user._id), 1);
+					thread.upvotes.push(req.user._id);
+					thread.score -= 1;
+					thread.save((err, item) => {
+						thread = thread.afterSave(req.user);
+
+						if (err) {
+							return json.bad(err, res);
+						}
+
+						json.good({
+							record: item
+						}, res);
+					});
+				} else {
+					thread.upvotes.push(req.user._id);
+					thread.save((err, item) => {
+						thread = thread.afterSave(req.user);
+
+						if (err) {
+							return json.bad(err, res);
+						}
+
+						json.good({
+							record: item
+						}, res);
+					});
+				}
+			}
 		});
 	};
 
