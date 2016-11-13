5addc275d521cdfbb6c5b577b61fc81df39621f5
Forrest Collins <Forest.d.collins@gmail.com>
2016-11-13 05:10:58 -0600

    Added chat create code in server controller

--- server.babel.js
+++ server.babel.js
@@ -8,7 +8,7 @@ import Streams from './server/models/streams'
 import Threads from './server/models/threads'
 import Comments from './server/models/comments'
 import Activity from './server/models/activities';
-
+import Chats from './server/models/chats'
 
 var config = require('./server/config/env/' + (process.env.NODE_ENV || 'development'));
 const db = mongoose.connect(config.db, () => {
