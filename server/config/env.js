7088b3d6f6186c6855761c9b0cc3769a2b3e6dd8
Forrest Collins <Forest.d.collins@gmail.com>
2016-11-20 16:18:00 -0600

    My domain is frustrating me. Waiting for it to update

--- server/config/env.js
+++ server/config/env.js
@@ -2,4 +2,4 @@ process.env["PORT"] = 8080,
 process.env["SECRET"] = 'asdkfjahsdkfhaskdfh'
 process.env["USER"] = "Forest.Collins.Portfolio@gmail.com"
 process.env["Pass"] = "Bashkir1693"
-process.env["MONGO_URI"] = "mongodb://Bashkir:Iloveemily1@ds011785.mlab.com:11785/opinionated"
+process.env["MONGO_URI"] = "mongodb://<Bashkir>:<Iloveemily1>@ds011785.mlab.com:11785/opinionated"
